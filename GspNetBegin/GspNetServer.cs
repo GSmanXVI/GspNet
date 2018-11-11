a19c770a5d7eb22ebebba42ae230cada8dd10e8c
Gleb Skripnikov <gspostmail@gmail.com>
2018-11-12 02:53:47 +0400

    Initial commit

---
+++ GspNetBegin/GspNetServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GspNetBegin
+{
+    class GspNetServer
+    {
+        public string Domain { get; set; }
+        public int Port { get; set; }
+
+        private HttpListener httpListener;
+
+        public GspNetServer(string domain, int port)
+        {
+            Domain = domain;
+            Port = port;
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add($"{domain}:{port}/");
+        }
+
+        public void Run()
+        {
+            httpListener.Start();
+            while (true)
+            {
+                var context = httpListener.GetContext();
+                Task.Run(() => Process(context));
+            }
+        }
+
+        private void Process(HttpListenerContext context)
+        {
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            StreamReader reader = new StreamReader(request.InputStream);
+            StreamWriter writer = new StreamWriter(response.OutputStream);
+
+            writer.WriteLine("Hello!");
+
+            writer.Close();
+        }
+    }
+}
---
+++ GspNetBegin/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GspNetBegin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new GspNetServer("http://+", 8080);
+            server.Run();
+        }
+    }
+}
